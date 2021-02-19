1a2afcdd708f564d02bb92a9a3f9dd3407499434
chamce <whitakerchancellor@yahoo.com>
2021-02-19 06:14:35 -0500

    countdown timer with basic style

---
+++ js/main.js
@@ -0,0 +1,85 @@
+const meeting = document.querySelector('.meeting');
+const countdown = document.querySelector('.countdown');
+
+function getCountdown() {
+    // am I in a meeting or when is my next meeting
+    let now = new Date();
+    let hour = now.getHours();
+    let day = now.getDay();
+    if (hour >= 8 && hour < 17 && day >= 1 && day <= 5) {
+        meeting.textContent = 'Your Awesome Inc meeting ends in:'
+        calculate(now, -1);
+    } else if (hour < 8 && day >= 1 && day <= 5) {
+        meeting.textContent = 'You have an Awesome Inc meeting today in:'
+        calculate(now, 0);
+    } else if (hour >= 17 && day === 5) {
+        meeting.textContent = 'You have an Awesome Inc meeting Monday in:'
+        calculate(now, 3);
+    } else if (day === 6) {
+        meeting.textContent = 'You have an Awesome Inc meeting Monday in:'
+        calculate(now, 2);
+    } else {
+        meeting.textContent = 'You have an Awesome Inc meeting tomorrow in:'
+        calculate(now, 1);
+    }
+}
+
+function calculate(now, days) {
+    if (days === -1) {
+        // get date of 17 o'clock today
+        let then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 17, 0, 0);
+        // get day, hour, minute, and second
+        let x = dhms(now, then);
+        // display timer
+        displayTimer(x[0], x[1], x[2], x[3]);
+    } else {
+        // get date of 8 o'clock in 3 days, in 2 days, tomorrow, or today
+        let then = new Date(now);
+        then.setDate(then.getDate() + days);
+        then = new Date(then.getFullYear(), then.getMonth(), then.getDate(), 8, 0, 0);
+        // get day, hour, minute, and second
+        let x = dhms(now, then);
+        // display timer
+        displayTimer(x[0], x[1], x[2], x[3]);
+    }
+}
+
+function dhms(now, then) {
+    // change dates to time
+    now = now.getTime();
+    then = then.getTime();
+    // get countdown time
+    let time = then - now;
+    // get day, hour, minute, and second of countdown time
+    let d = Math.floor(time / (1000 * 60 * 60 * 24));
+    let h = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let m = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    let s = Math.floor((time % (1000 * 60)) / 1000);
+    return [d, h, m, s];
+}
+
+function displayTimer(d, h, m, s) {
+    // if day, hour, minute, or second < 10
+    if (d < 10 || h < 10 || m < 10 || s < 10) {
+        // add a 0 in front of them
+        if (d < 10) {
+            d = '0' + d;
+        }
+        if (h < 10) {
+            h = '0' + h;
+        }
+        if (m < 10) {
+            m = '0' + m;
+        }
+        if (s < 10) {
+            s = '0' + s;
+        }
+    }
+    // display time
+    countdown.textContent = d + 'd ' + h + 'h ' + m + 'm ' + s + 's ';
+}
+
+// get countdown when page loads
+getCountdown();
+// get countdown every second
+setInterval(getCountdown, 1000);
